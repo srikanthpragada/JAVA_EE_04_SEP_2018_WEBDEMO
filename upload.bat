34773d3ae73f5e1b4b87d348ae053b3f992a8d80
srikanthpragada <srikanthpragada@gmail.com>
2018-09-10 09:06:48 +0530

    10-SEP

---
+++ upload.bat
@@ -0,0 +1,10 @@
+rem add all files
+git add .
+
+rem commit with the given message 
+git commit -m %1
+
+rem push master branch of this remote project
+git push java_ee_04_sep_2018_webdemo master
+
+
